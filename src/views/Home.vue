15819114827ad92907990da5f158a189d0b51b3c
smallunoversezzq <15003873032@163.com>
2020-01-19 23:43:43 +0800

    增加axios请求

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,6 +7,7 @@
 
 <script>
 import Nav from '@/components/nav.vue'
+import { getHeweather } from '@/api/home/index.js'
 export default {
   name: "home",
   components: {
@@ -18,9 +19,18 @@ export default {
     }
   },
   methods:{
-
   },
-  ceated(){},
+  created(){
+    getHeweather({
+      location:'beijing',
+      key:'8dda60d1afc549339bf5d0964955ce91'
+    }).then(res =>{
+      console.log(res)
+      if(res.status === 200){
+        console.log(res.data.HeWeather6[0].basic);
+      }
+    })
+  },
   mounted(){},
   updated(){},
 };
